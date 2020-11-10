7979fb7c616f04361fe1b137b8e99e38547c8ef2
arif <arif@bitmascot.com>
2020-11-10 16:57:17 +1100

    done

---
+++ src/test/java/com/autobill/connect/APIConfigTest.java
@@ -0,0 +1,13 @@
+package com.autobill.connect;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class APIConfigTest {
+    @Test
+    public void testAPICOnfig(){
+        assert 1==1;
+    }
+
+}
